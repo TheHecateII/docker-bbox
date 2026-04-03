04f1552c7f3800a257e4c4402e64c9d26effbfa4
Hecate <83183415+TheHecateII@users.noreply.github.com>
2026-04-03 23:23:28 +0200

    Refactor IPv6 address configuration logic for more flexibility
    Update IPv6 address assignment and logging.

--- ipv6-dhcp-exit-hook.sh
+++ ipv6-dhcp-exit-hook.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-
 if [ -n "$new_ip6_prefix" ]; then
-    base_prefix=$(echo "$new_ip6_prefix" | sed -E 's/.{7}$//')
-
+    prefix_root=$(echo "$new_ip6_prefix" | cut -d'/' -f1 | sed 's/::$//')
     ip -6 address flush dev $WAN_INTERFACE.100 scope global
     ip -6 address flush dev $LAN_INTERFACE scope global
-
-    ip address add "$base_prefix"01::1/64 dev $WAN_INTERFACE.100
-    ip address add "$base_prefix"02::1/64 dev $LAN_INTERFACE
-    echo "Updated IPv6 addresses"
+    ip address add "${prefix_root%?}1::1/64" dev $WAN_INTERFACE.100
+    ip address add "${prefix_root%?}2::1/64" dev $LAN_INTERFACE
+    echo "IPv6 configurée sur le préfixe ${prefix_root}"
 fi
